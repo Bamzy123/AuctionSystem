10dc2b7c47cae2e58a0be6231c2d550c41a92de2
Bamzy123 <stephenomotos@gmail.com>
2025-04-18 14:38:26 +0100

    auction update 001

---
+++ src/main/java/org/auctionsystem/models/AuctionItem.java
@@ -0,0 +1,37 @@
+package org.auctionsystem.models;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+
+@Document(collection = "auctionItems")
+public class AuctionItem extends User{
+    @Id
+    private String itemId;
+    private String title;
+    private String description;
+    private double startingPrice;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private String status;
+
+    public AuctionItem() {}
+
+    public AuctionItem(String itemId, String title, String description,
+                       double startingPrice, LocalDateTime startTime,
+                       LocalDateTime endTime, String status) {
+        this.itemId = itemId;
+        this.title = title;
+        this.description = description;
+        this.startingPrice = startingPrice;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.status = status;
+    }
+}
---
+++ src/main/java/org/auctionsystem/models/User.java
@@ -0,0 +1,27 @@
+package org.auctionsystem.models;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Setter
+@Getter
+@Document(collection = "users")
+public abstract class User {
+    @Id
+    private String userId;
+    private String username;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String userId, String username, String password) {
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+    }
+
+}
+
